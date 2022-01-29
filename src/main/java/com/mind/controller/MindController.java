bd417a82ff9c1cc5e3fd759d82e4091539441be9
Nagesh Thota <nageshthota96@gmail.com>
2022-01-29 22:25:45 +0530

    mind

--- src/main/java/com/mind/controller/MindController.java
+++ src/main/java/com/mind/controller/MindController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("/")
 public class MindController {
 
 	@RequestMapping(value="/home",method = RequestMethod.GET)
